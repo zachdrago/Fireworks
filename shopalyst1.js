cb573f3aded28730141d9c0143785b1cbba41299
Zach Drago <zachdrago@gmail.com>
2018-06-28 11:54:25 -0400

    Shopalyst JS
    Shopalyst JS

---
+++ shopalyst1.js
@@ -0,0 +1,5 @@
+var _shopalyst = {
+    pub: 'stackla',
+    pl: 22815,
+    cm: 22815
+};
---
+++ shopalyst2.js
@@ -0,0 +1,15 @@
+// Setup the display properties for the widget (such as the container it must render in)
+var displayOptions = {
+    //The CSS selector of the element to embed the widget into. 
+    //It is recommended that the container has it's height set to 250px and the width to 100%. 
+    //The embed is set to occupy 100% width and height of the parent container.
+    container: '.stacklapopup-products-item' 
+};
+// Call openBinWidget to display the BIN widget in the specified container
+_shopalyst.openBinWidget('8712561977562', displayOptions);
+
+// To open the bin widget on the click of a button, invoke it in the click handler of the desired element
+button.addEventListener('click',function(){
+    _shopalyst.openBinWidget('8712561977562', displayOptions);
+});
+
