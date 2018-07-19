500ff39bbe21afc76cf71697e132824ec0ad8d36
Zach Drago <zachdrago@gmail.com>
2018-07-18 20:00:38 -0400

    Create video_controls.js
    video controls JS

---
+++ video_controls.js
@@ -0,0 +1,16 @@
+var vid = $('#background_video');
+var played = $("#play");
+var paused = $("#pause");
+vid.volume = 0.00;
+
+function pauseVid() {
+  vid.pause();
+  paused.style.opacity = 0.4;
+  played.style.opacity = 1.0;
+}
+
+function playVid() {
+  vid.play();
+  played.style.opacity = 0.4;
+  paused.style.opacity = 1.0;
+}
