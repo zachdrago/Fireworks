7ab3469467de16d9efc31be898f8ff7ae00079ad
Zach Drago <zachdrago@gmail.com>
2018-07-18 20:04:21 -0400

    Update video_controls.js
    update vid controls

--- video_controls.js
+++ video_controls.js
@@ -1,29 +1,16 @@
-// var vid = $('#background_video');
-// var played = $("#play");
-// var paused = $("#pause");
-// vid.volume = 0.00;
+var vid = $('#background_video');
+var played = $("#play");
+var paused = $("#pause");
+vid.volume = 0.00;
 
-// function pauseVid() {
-//   vid.pause();
-//   paused.style.opacity = 0.4;
-//   played.style.opacity = 1.0;
-// }
+function pauseVid() {
+  vid.pause();
+  paused.style.opacity = 0.4;
+  played.style.opacity = 1.0;
+}
 
-// function playVid() {
-//   vid.play();
-//   played.style.opacity = 0.4;
-//   paused.style.opacity = 1.0;
-// }
-
-
-var video = $('#background_video');
-
-$('#play').on('click', function() {
-    if(video.paused) {
-        video.play();
-    }
-    else {
-        video.pause();
-    }
-    return false;
-};
+function playVid() {
+  vid.play();
+  played.style.opacity = 0.4;
+  paused.style.opacity = 1.0;
+}
